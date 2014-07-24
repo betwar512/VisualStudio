9fba0c3ea205a275cc5095fddb9fb4ef858ddc12
betwar512 <betwar512@live.com>
2014-07-24 21:28:07 +1000

    this si first commit

---
+++ MobileServiceTest/DataObjects/TodoItem.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.WindowsAzure.Mobile.Service;
+
+namespace MobileServiceTest.DataObjects
+{
+    public class TodoItem : EntityData
+    {
+        public string Text { get; set; }
+
+        public bool Complete { get; set; }
+    }
+}
